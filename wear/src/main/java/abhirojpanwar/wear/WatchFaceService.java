764a6ee4f79cbc27e03f8eeafdca7bd610d58489
Abhiroj Panwar <abhiroj95@gmail.com>
2017-02-16 00:26:48 +0530

    feat: Add watchfaceservice

---
+++ wear/src/main/java/abhirojpanwar/wear/WatchFaceService.java
@@ -0,0 +1,109 @@
+package abhirojpanwar.wear;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.view.SurfaceHolder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Abhiroj on 2/15/2017.
+ */
+
+public class WatchFaceService extends CanvasWatchFaceService {
+    @Override
+    public Engine onCreateEngine() {
+        return new WatchEngine();
+    }
+
+    class WatchEngine extends CanvasWatchFaceService.Engine{
+
+        private final long TICK_PERIOD_MILLIS = TimeUnit.SECONDS.toMillis(1);
+        private Handler timeTick;
+        private SimpleWatchFace watchFace;
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+
+            setWatchFaceStyle(new WatchFaceStyle.Builder(WatchFaceService.this)
+                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_SHORT)
+                    .setAmbientPeekMode(WatchFaceStyle.AMBIENT_PEEK_MODE_HIDDEN)
+                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
+                    .setShowSystemUiTime(false)
+                    .build());
+
+            timeTick = new Handler(Looper.myLooper());
+            startTimerIfNecessary();
+            watchFace = SimpleWatchFace.newInstance(WatchFaceService.this);
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            super.onDraw(canvas, bounds);
+            watchFace.draw(canvas, bounds);
+        }
+
+
+        private void startTimerIfNecessary() {
+            timeTick.removeCallbacks(timeRunnable);
+            if (isVisible() && !isInAmbientMode()) {
+                timeTick.post(timeRunnable);
+            }
+        }
+
+        private final Runnable timeRunnable = new Runnable() {
+            @Override
+            public void run() {
+                onSecondTick();
+
+                if (isVisible() && !isInAmbientMode()) {
+                    timeTick.postDelayed(this, TICK_PERIOD_MILLIS);
+                }
+            }
+        };
+
+        private void onSecondTick() {
+            invalidateIfNecessary();
+        }
+
+        private void invalidateIfNecessary() {
+            if (isVisible() && !isInAmbientMode()) {
+                invalidate();
+            }
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+            startTimerIfNecessary();
+        }
+
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            super.onAmbientModeChanged(inAmbientMode);
+            watchFace.setAntiAlias(!inAmbientMode);
+            watchFace.setColor(inAmbientMode ? Color.GRAY : Color.WHITE);
+            watchFace.setShowSeconds(!isInAmbientMode());
+            invalidate();
+            startTimerIfNecessary();
+        }
+
+        @Override
+        public void onTimeTick() {
+            super.onTimeTick();
+            invalidate();
+        }
+
+        @Override
+        public void onDestroy() {
+            timeTick.removeCallbacks(timeRunnable);
+            super.onDestroy();
+        }
+    }
+}
